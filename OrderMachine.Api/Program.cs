093c6feba89edb1eff46d20bfa48de65f1ff758e
Geert Van Laethem <geert.vanlaethem@egemin.be>
2016-05-02 21:21:47 +0200

    selfhost and first controller set

---
+++ OrderMachine.Api/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Owin.Hosting;
+
+namespace OrderMachine.Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000";
+            WebApp.Start<Startup>(url: baseAddress);
+            Console.ReadKey();
+        }
+    }
+}
--- OrderMachine/Product.cs
+++ OrderMachine/Product.cs
@@ -1,6 +1,6 @@
 ﻿namespace OrderMachine
 {
-    public class Product
+    public class Product : IProduct
     {
         public string Name { get; set; }
         public string Description { get; set; }
