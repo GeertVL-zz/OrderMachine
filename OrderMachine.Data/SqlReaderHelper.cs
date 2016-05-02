2c9549fa9aed32d9695e75a009dfa9562ad6ac1a
Geert Van Laethem <geert.vanlaethem@egemin.be>
2016-05-02 13:58:30 +0200

    SqlReaderHelper created in its first attempt with accompanying tests

---
+++ OrderMachine.Data/SqlReaderHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+
+namespace OrderMachine.Data
+{
+    public static class SqlReaderHelper
+    {
+        public static T Q<T>(this IDataReader reader, string name)
+        {
+            return (T)reader[name];
+        }
+
+        public static void Q<T>(this SqlCommand command, string name, T value)
+        {
+            command.Parameters.Add(new SqlParameter("@" + name, value));
+        }
+
+        public static SqlCommand Q<T>(this SqlConnection connection, string name)
+        {
+            var command = new SqlCommand(name, connection) { CommandType = CommandType.StoredProcedure };
+            return command;
+        }
+
+    }
+}
---
+++ OrderMachine.Tests/SqlReaderHelperTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using FakeItEasy;
+using Xunit;
+using OrderMachine.Data;
+
+namespace OrderMachine.Tests
+{
+    [Trait("Category", "SqlReaderHelper")]
+    public class SqlReaderHelperTests
+    {
+
+        [Fact(DisplayName = "Check if you can read values")]
+        public void SpecificationName()
+        {
+            var connection = A.Fake<IDbConnection>();
+            var command = connection.CreateCommand();
+            var reader = command.ExecuteReader();
+            A.CallTo(() => reader["name"]).Returns("test");
+            A.CallTo(() => reader["description"]).Returns("test");
+            var product = new Product
+            {
+                Name = reader.Q<string>("name"),
+                Description = reader.Q<string>("description")
+            };
+
+            Assert.Equal(product.Name, "test");
+        }
+    }
+}
